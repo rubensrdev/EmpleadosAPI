177a892f60e685ee7ad76b5d14c5ebddd76bf448
Rubén <ruben.sr.apple@gmail.com>
2024-12-06 12:47:48 +0100

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Rubén Segura Romo on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

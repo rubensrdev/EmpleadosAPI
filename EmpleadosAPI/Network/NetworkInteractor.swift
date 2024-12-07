1858c44fb3b1b4c336bb6a51375ebeda563d191e
Rubén <ruben.sr.apple@gmail.com>
2024-12-07 18:37:58 +0100

    Refactorizando para un mejor entendimiento

--- EmpleadosAPI/Network/NetworkInteractor.swift
+++ EmpleadosAPI/Network/NetworkInteractor.swift
@@ -12,16 +12,17 @@ protocol NetworkInteractor {}
 
 // Proporciona métodos genéricos para realizar peticiones GET y POST
 extension NetworkInteractor {
-	/// Este método genérico realiza una solicitud HTTP de tipo GET y decodifica la respuesta JSON en un tiop Codable
+	/// Este método genérico ejecuta una solicitud HTTP y decodifica la respuesta JSON en un tipo `Codable`.
 	/// - Parámetros:
-	///   - `request`: La solicitud HTTP a ejecutar.
+	///   - `request`: La solicitud HTTP a ejecutar (GET, POST, DELETE, PUT)
 	///   - `type`: El tipo al que se desea decodificar la respuesta JSON.
 	/// - Retorno:
 	///   - Un objeto decodificado del tipo especificado (`JSON`).
 	/// - Errores:
 	///   - Lanza `NetworkError.status` si el código de estado HTTP no es 200.
 	///   - Lanza `NetworkError.json` si la decodificación JSON falla.
-	func getJSON<JSON>(request: URLRequest, type: JSON.Type) async throws(NetworkError) -> JSON where JSON: Codable {
+	///   - Lanza `NetworkError.general` para errores generales durante la ejecución de la solicitud.
+	func executeRequest<JSON>(request: URLRequest, type: JSON.Type) async throws(NetworkError) -> JSON where JSON: Codable {
 		let (data, response) = try await URLSession.shared.getData(for: request)
 		if response.statusCode == 200 {
 			do {

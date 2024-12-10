b294470d86a6e6e06e54aaa2e1b34d87278d8a75
Rubén <ruben.sr.apple@gmail.com>
2024-12-10 13:05:27 +0100

    nota sobre async image en swift 6

--- EmpleadosAPI/View/Components/EmpleadoAsyncImage.swift
+++ EmpleadosAPI/View/Components/EmpleadoAsyncImage.swift
@@ -9,6 +9,14 @@ import SwiftUI
 struct EmpleadoAsyncImage: View {
 	let empleado: Empleado
 	var body: some View {
+		 /*
+		  El uso de AsyncImage conlleva un problema ahora en Swift 6
+		  que es la llamada "Re-Entrada del Actor":
+		  Cuando se solitica el mismo resultado de la misma tarea
+		  más de una vez antes de que la primera vez se descargue
+		  y eso puede provocar un error redundante que bloquea la app
+		  o que nunca llegue a descargar la imagen
+		  */
 		AsyncImage(url: empleado.avatar){ image in
 			if let image = image.image {
 				image

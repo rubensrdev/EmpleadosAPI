fb1e8ce32bb68b99847e57c07fb89ddc27ef1a62
Rubén <ruben.sr.apple@gmail.com>
2024-12-13 19:56:46 +0100

    Primer campo editable

--- EmpleadosAPI/ViewModel/EmpleadoEditViewModel.swift
+++ EmpleadosAPI/ViewModel/EmpleadoEditViewModel.swift
@@ -11,7 +11,23 @@ import Foundation
 final class EmpleadoEditViewModel {
 	let empleado: Empleado
 	
-	init(empleado: Empleado) {
+	var firstName: String
+	var lastName: String
+	var email: String
+	var username: String
+	var address: String
+	var department: Empleado.Departamento
+	var gender: Empleado.Genero
+	
+	init (empleado: Empleado) {
 		self.empleado = empleado
+		
+		firstName = empleado.firstName
+		lastName = empleado.lastName
+		email = empleado.email
+		username = empleado.username
+		address = empleado.address
+		department = empleado.department
+		gender = empleado.gender
 	}
 }
